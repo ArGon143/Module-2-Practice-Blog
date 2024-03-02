3b186135764c499d5ee6cd17f7f94cc17368f948
argon <argon143@mail.ru>
2024-03-02 14:27:59 +0300

    Реализовать редактирование статьи

--- my-app/src/pages/post/post.js
+++ my-app/src/pages/post/post.js
@@ -1,17 +1,18 @@
 import styled from 'styled-components';
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { Comments, PostContent } from './components';
-import { useParams } from 'react-router-dom';
+import { Comments, PostContent, PostForm } from './components';
+import { useMatch, useParams } from 'react-router-dom';
 import { useServerRequest } from '../../hooks';
 import { loadPostAsync } from '../../actions';
 import { selectorPost } from '../../selectors';
 
 const PostContainer = ({ className }) => {
-	const post = useSelector(selectorPost);
 	const dispatch = useDispatch();
 	const params = useParams();
+	const isEditing = useMatch('/post/:id/edit');
 	const requestServer = useServerRequest();
+	const post = useSelector(selectorPost);
 
 	useEffect(() => {
 		dispatch(loadPostAsync(requestServer, params.id));
@@ -19,8 +20,14 @@ const PostContainer = ({ className }) => {
 
 	return (
 		<div className={className}>
-			<PostContent post={post} />
-			<Comments comments={post.comments} postId={post.id} />
+			{isEditing ? (
+				<PostForm post={post} />
+			) : (
+				<>
+					<PostContent post={post} />
+					<Comments comments={post.comments} postId={post.id} />
+				</>
+			)}
 		</div>
 	);
 };
